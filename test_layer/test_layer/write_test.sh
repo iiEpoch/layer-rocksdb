6ff624af690d3d6bb28284d9e243c4e882c59dd2
JingYu <92147862+iiEpoch@users.noreply.github.com>
2023-01-14 12:40:53 +0800

    Add files via upload

---
+++ test_layer/test_layer/write_test.sh
@@ -0,0 +1,39 @@
+#db_path
+db_path="./db_test"
+
+#pmem_path
+db_data_pmem="./pmem_test"
+
+#ssd_path
+db_data_ssd="./ssd_test"
+
+#wal_path
+wal_path="./db_test"
+
+#report path
+report_path="/mnt/222"
+
+#layer_level
+layer_level=2
+
+threads=1
+
+back_g=24
+
+for j in {56,112} #dataset
+do
+	for i in {256,512,1024,4096}  #value_size
+	do
+		
+		#准备
+		./write_layer.sh ${i} 1 ${back_g} ${j} ${db_path} ${db_data_pmem} ${db_data_ssd} ${wal_path} ${report_path} ${layer_level}
+
+		 sleep 300
+
+		#测试
+		./write_layer.sh ${i} ${threads} ${back_g} ${j} ${db_path} ${db_data_pmem} ${db_data_ssd} ${wal_path} ${report_path} ${layer_level}
+
+		 sleep 300
+
+	done
+done
