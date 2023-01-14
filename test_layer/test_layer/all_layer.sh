b967b75f937a88bdb17237d02bd428f8975b5b64
JingYu <92147862+iiEpoch@users.noreply.github.com>
2023-01-14 12:52:17 +0800

    Add files via upload

---
+++ test_layer/test_layer/all_layer.sh
@@ -0,0 +1,45 @@
+#db_path
+path="./db_test"
+
+#pmem_path
+path1="./pmem_test"
+
+#ssd_path
+path2="./ssd_test"
+
+#wal_path(pmem)
+path3="./db_test"
+
+#wal_path(ssd)
+path4="./db_test"
+
+#report path
+path5="/mnt/222"
+
+threads=1
+
+back_g=24
+
+#write参数：value_size threads max_backgroungjobs dataset db path_pmem path_ssd wal_path report_file
+
+#read参数：value_size reads max_backgroungjobs duration dataset  db path_pmem path_ssd wal_path use_direct_read read_cache_size report_file
+
+#readrandomwriterandom参数:value_size threads max_backgroungjobs dataset duration db path_pmem path_ssd wal_path use_direct_read read_cache_size report_file
+
+#准备
+
+./write_layer.sh 128 ${threads} ${back_g} 2 ${path} ${path1} ${path2} ${path3} ${path5} & sleep 300
+
+for j in {56,112}
+do
+	for i in {256,512,1024,4096}
+	do
+
+		./write_layer.sh ${i} ${threads} ${back_g} ${j} ${path} ${path1} ${path2} ${path3} ${path5} & sleep 300
+
+		./read_layer.sh ${i} ${threads} ${back_g} 14400 ${j} ${path} ${path1} ${path2} ${path3} false 0 ${path5} & sleep 300
+
+		./read_layer.sh ${i} ${threads} ${back_g} 14400 ${j} ${path} ${path1} ${path2} ${path3} true 10 ${path5} & sleep 300	
+	done
+done
+
